95fcc8db45175b5c209d880a77df84b58a164546
yumimts.aile.0407.0318@gmail.com <yumimts.aile.0407.0318@gmail.com>
2023-11-26 19:38:38 +0900

    first commit

---
+++ kadai_004/event.js
@@ -0,0 +1,11 @@
+//HTMLドキュメントが読み込み完了したときに「loadイベントが発生しました」とコンソールに出力する
+//画面をスクロールしたときに「scrollイベントが発生しました」とコンソールに出力する
+
+
+$(window).on('load', function() {
+    console.log("loadイベントが発生しました");
+});
+
+$(window).on('scroll', function() {
+    console.log("scrollイベントが発生しました");
+});
